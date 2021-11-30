4587bd2bfcf2f80ac126dc863da5eeaaa26275af
Jake <jakerfilut@gmail.com>
2021-11-30 15:38:44 -0700

    Completed Lab

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,32 @@
 import React from "react";
+import Task from "./Task"
 
-function TaskList() {
+function TaskList({handleDelete, tasksDisplay}) {
+const mapTasks = tasksDisplay.map((task) => (
+  <Task key={task.text}
+  text={task.text}
+  category={task.category}
+  handleDelete={handleDelete}/>
+))
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {mapTasks}
     </div>
   );
 }
 
 export default TaskList;
+
+
+
+// import React from "react";
+
+// function TaskList() {
+//   return (
+//     <div className="tasks">
+//       {/* display a list of tasks using Task component */}
+//     </div>
+//   );
+// }
+
+// export default TaskList;
